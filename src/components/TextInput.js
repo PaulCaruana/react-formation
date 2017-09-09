a27e66191800ce3b252ab89a22c38e90404b52b8
PaulCaruana <pgcaruana@optusnet.com.au>
2017-09-09 19:21:27 +1000

    Streamline code

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -1,21 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import TextField from 'material-ui/TextField';
 import Conditional from './Conditional';
 import Field from './Field';
 import componentProps from 'verity/componentProps';
 
-class TextInput extends React.Component {
+class TextInput extends Component {
     render() {
-        const props = componentProps(TextField, this.props)
+        const FormComponent = TextField;
         return (
             <div>
-                <div>
-                    <TextField
-                        hintText={this.props.placeholder}
-                        floatingLabelText={this.props.label}
-                        {...props}
-                    />
-                </div>
+                <FormComponent
+                    hintText={this.props.placeholder}
+                    floatingLabelText={this.props.label}
+                    {...componentProps(FormComponent, this.props)}
+                />
             </div>
         );
     }

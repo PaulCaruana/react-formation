06d708ebc42ff611cab8c977ee458c228a7d0c0b
PaulCaruana <pgcaruana@optusnet.com.au>
2017-09-18 16:02:49 +1000

    Refactor widgets

--- src/components/TextInput.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import TextField from 'material-ui/TextField';
-import { Conditional, Field } from 'react-formwork';
-
-export default Field(TextField, {
-    hintText: props => props.placeholder,
-    floatingLabelText: props => props.label
-});

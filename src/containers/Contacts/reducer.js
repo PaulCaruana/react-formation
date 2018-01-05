c9c79d02541b879eaff4a7ee4761443151cd1ad6
PaulCaruana <pgcaruana@optusnet.com.au>
2018-01-06 08:53:21 +1100

    Cleanup reducers

--- src/containers/Contacts/reducer.js
+++ src/containers/Contacts/reducer.js
@@ -1,14 +1,23 @@
+import { combineReducers } from "redux";
 import { handleActions } from 'redux-actions';
 import * as actions from './constants';
+import formationReducer from 'react-formation/reducer';
+import { formName as homePageForm, pageName as homePage } from './index';
 
 const initialState = {
     response: {}
 };
 
-export default handleActions({
+const reducer = handleActions({
     [actions.CREATE]: (state, action) => {
         const newState = { ...state };
         newState[action.payload.name] = action.payload.value;
         return newState;
     }
 }, initialState);
+
+export default combineReducers({
+    [homePage]: reducer,
+    [homePageForm]: formationReducer
+});
+
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,3 @@
-import { combineReducers } from "redux";
-import reduceReducers from 'reduce-reducers';
-import formationReducer from 'react-formation/reducer';
-import homeReducer from '../containers/Contacts/reducer';
-import { formName as homePageForm, pageName as homePage } from '../containers/Contacts';
+import reducer from '../containers/Contacts/reducer';
 
-const rootReducer = combineReducers({
-    [homePage]: homeReducer,
-    [homePageForm]: formationReducer
-});
-
-export default rootReducer;
+export default reducer;

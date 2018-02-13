3d9fcef38abba16c90d821469954305762b106ea
Paul Caruana <paul.caruana@news.com.au>
2018-02-14 08:52:00 +1100

    fix error

--- src/index.js
+++ src/index.js
@@ -4,4 +4,3 @@ export { default as Field } from './components/Field';
 export { default as SimpleList } from './components/SimpleList';
 export { default as mapProps } from './mapProps';
 export { default as reducer } from './reducer';
-export { default as TextField } from './TextField';

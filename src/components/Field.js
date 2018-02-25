9121ec7ad1a2cfe15f20fa056b7b29c6ad8d15b2
PaulCaruana <pgcaruana@optusnet.com.au>
2018-02-25 15:43:54 +1100

    Interim fix

--- src/components/Field.js
+++ src/components/Field.js
@@ -6,7 +6,7 @@ import FieldController from './controllers/field';
 import SimpleList from './SimpleList';
 import mapProps from '../mapProps';
 import rootProps from '../rootProps';
-import equals from 'fast-deep-equal';
+import equals from 'shallow-equals';
 import debounce from 'lodash.debounce';
 
 export default (BaseComponent, mapper = null) => class extends Component {
